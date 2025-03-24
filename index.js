d536294188c1894eb349c9de7d2e26d641e2dd99
izzatullohdev <izzatbekmadaminov7141@gmail.com>
2025-03-24 23:25:14 +0500

    Sequelize ORM 3-dars: findAll, findOne, findByPk orqali foydalanuvchini olish | CRUD Read bosqichi

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const sequelize = require("./config/database");
 const User = require("./models/user.model");
 (async () => {
@@ -11,13 +12,86 @@ const User = require("./models/user.model");
   }
 })();
 
+// (async () => {
+//   try {
+//     const newUser = await User.create({
+//       username: "holid28",
+//       email: "holid31@example.com",
+//     });
+//     console.log("Yangi foydalanuvchi yaratildi:", newUser.toJSON());
+//   } catch (err) {
+//     console.error("Xatolik yuz berdi :", err.message);
+//   }
+// })();
+
+// findAll()
+// (async () => {
+//   try {
+//     const users = await User.findAll({
+//       where: { username: "holid28" },
+//       limit: 2,
+//       order: [["createdAt", "DESC"]],
+//       attributes: ["id", "username"],
+//     });
+//     console.log(
+//       "Foydalanuvchilar ro'yxati:",
+//       users.map((u) => u.toJSON())
+//     );
+//   } catch (err) {
+//     console.error("Xatolik yuz berdi :", err.message);
+//   }
+// })();
+
+// findOne()
+// (async () => {
+//   try {
+//     const user = await User.findOne({
+//       where: { email: "holid32@example.com" },
+//     });
+//     console.log("Topilgan foydalanuvchi:", user?.toJSON());
+//   } catch (err) {
+//     console.error("Xatolik yuz berdi :", err.message);
+//   }
+// })();
+
+// findByPk();
+// (async () => {
+//   try {
+//     const user = await User.findByPk(100);
+//     if (!user) {
+//       console.log("Foydalanuvchi topilmadi");
+//     } else {
+//       console.log("ID bo‘yicha topilgan foydalanuvchi:", user.toJSON());
+//     }
+//   } catch (err) {
+//     console.error("Xatolik yuz berdi :", err.message);
+//   }
+// })();
+
+// count()
+// (async () => {
+//   try {
+//     const count = await User.count();
+//     console.log("Jami foydalanuvchilar soni:", count);
+//   } catch (err) {
+//     console.error("Xatolik yuz berdi :", err.message);
+//   }
+// })();
+
 (async () => {
   try {
-    const newUser = await User.create({
-      username: "holid28",
-      email: "holid28@example.com",
+    const gmailUsers = await User.count({
+      where: {
+        email: {
+          [Op.like]: "%@example.com",
+        },
+      },
     });
-    console.log("Yangi foydalanuvchi yaratildi:", newUser.toJSON());
+    if (gmailUsers > 0) {
+      console.log("Example foydalanuvchilari soni:", gmailUsers);
+    } else {
+      console.log("Example foydalanuvchilari yo'q");
+    }
   } catch (err) {
     console.error("Xatolik yuz berdi :", err.message);
   }
